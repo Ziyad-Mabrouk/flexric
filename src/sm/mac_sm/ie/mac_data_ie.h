1e5b0a288163df8dfaa6ef874352c3b0971fb2af
Ziyad Mabrouk <ziyadmabrouk@outlook.com>
2025-05-12 15:39:19 +0200

    add logging interval to mac control

--- src/sm/mac_sm/ie/mac_data_ie.h
+++ src/sm/mac_sm/ie/mac_data_ie.h
@@ -180,6 +180,7 @@ bool eq_mac_ctrl_hdr(mac_ctrl_hdr_t* m0, mac_ctrl_hdr_t* m1);
 
 typedef struct {
   uint32_t action;
+  uint32_t log_interval_ms;
 } mac_ctrl_msg_t;
 
 void free_mac_ctrl_msg( mac_ctrl_msg_t* src); 
